ade7067802037930d9299fc49b159c5c722bea0d
andreibac <85921833+andreibac@users.noreply.github.com>
2022-04-26 13:44:19 +0300

    Create guessTheButton.js

---
+++ guessTheButton.js
@@ -0,0 +1,10 @@
+function checkWinner(clicked) {
+  var winner = Math.floor(Math.random() * 3 + 1);
+  var button = clicked;
+  if (button == winner) {
+    alert("CONGRATULATIONS, you guessed the lucky button !");
+  } else {
+    alert ("Sorry, the lucky button was Button " + winner);
+  }
+  return false;
+}
