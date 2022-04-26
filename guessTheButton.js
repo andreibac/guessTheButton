dfabb079924f9c6125afa5cab758803da77312ca
andreibac <85921833+andreibac@users.noreply.github.com>
2022-04-26 19:46:04 +0300

    Update guessTheButton.js

--- guessTheButton.js
+++ guessTheButton.js
@@ -1,10 +1,17 @@
-function checkWinner(clicked) {
-  var winner = Math.floor(Math.random() * 3 + 1);
-  var button = clicked;
-  if (button == winner) {
-    alert("CONGRATULATIONS, you guessed the lucky button !");
-  } else {
-    alert ("Sorry, the lucky button was Button " + winner);
+<!-- Option 2: Separate Popper and Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+<script>
+  function checkWinner(clicked) {
+    var winner = Math.floor(Math.random() * 3 + 1);
+    var button = clicked;
+    if (button == winner) {
+      alert("CONGRATULATIONS, you guessed the lucky button !");
+    } else {
+      alert ("Sorry, the lucky button was Button " + winner);
+    }
+    
+    return false;
   }
-  return false;
-}
+</script>
